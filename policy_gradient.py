447e7cfd0ca99a4eb70951a1c678d1cd03f24ea1
Kenny Song <kenny.ysong@gmail.com>
2017-02-22 19:42:10 +0800

    Cleaned up code

--- policy_gradient.py
+++ policy_gradient.py
@@ -162,7 +162,7 @@ def run_policy_network(policy_net, state):
 
         # Calculate grad_W(log(p)), for all parameters W
         log_p = dist[0][a_index].log()
-        log_p.backward()
+        log_p.backward()  # This will accumulate the gradient over all iterations
 
         # Track output of this iteration/agent
         action.append(actions[a_index])
@@ -173,6 +173,7 @@ def run_policy_network(policy_net, state):
         a_n = np.zeros(3)
         a_n[a_index] = 1
 
+    # Get the gradients; clone() is needed as the parameter Tensors are reused
     grad_W = [W.grad.data.clone() for W in policy_net.parameters()]
 
     return np.array(action), grad_W
@@ -208,28 +209,16 @@ def train_policy_network(policy_net, episode, baseline=None, lr=3*1e-3):
     for i, W in enumerate(policy_net.parameters()):
         W.data += lr * W_step[i] / (W_step[i].abs() + 1e-5)
 
-for i in range(1000):
-    policy_net = build_policy_network()
-    value_net = build_value_network()
-    baseline = lambda state: run_value_network(value_net, state)
+policy_net = build_policy_network()
+value_net = build_value_network()
+baseline = lambda state: run_value_network(value_net, state)
 
-    cum_value_error = 0.0
-    cum_return = 0.0
-    for num_episode in range(10000):
-        episode = run_episode(policy_net, gamma=1)
-        value_error = train_value_network(value_net, episode)
-        cum_value_error = 0.9 * cum_value_error + 0.1 * value_error
-        cum_return = 0.9 * cum_return + 0.1 * episode[0].G
-        # print("Num episode:{} Episode Len:{} Return:{} Baseline error:{}".format(num_episode, len(episode), cum_return, cum_value_error)) # Print episode return
-        train_policy_network(policy_net, episode, baseline=baseline)
-
-        if num_episode % 100 == 0: print('EPISODE {} RETURN {}'.format(num_episode, cum_return))
-        if cum_return == -101.0: print('DID NOT LEARN.')
-
-        if cum_return > -5.0:
-            print('Iteration {}, Episode {}, cum_return {}, cum_value_error {}'.format(i, num_episode, cum_return, cum_value_error))
-            break
-
-    if num_episode == 9999: 
-        print('DID NOT LEARN.')
-
+cum_value_error = 0.0
+cum_return = 0.0
+for num_episode in range(10000):
+    episode = run_episode(policy_net, gamma=1)
+    value_error = train_value_network(value_net, episode)
+    cum_value_error = 0.9 * cum_value_error + 0.1 * value_error
+    cum_return = 0.9 * cum_return + 0.1 * episode[0].G
+    print("Num episode:{} Episode Len:{} Return:{} Baseline error:{}".format(num_episode, len(episode), cum_return, cum_value_error)) # Print episode return
+    train_policy_network(policy_net, episode, baseline=baseline)
